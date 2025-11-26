e15e9eea2d7f54ea3eef94231b972596a1d26275
Jay <jayrathore88155@gmail.com>
2025-11-26 17:23:13 +0530

    Fix package.json

--- templates/src/index.ts
+++ templates/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { userRouter } from "./routes/userRoutes";
 
 const app = express();
 const PORT = 3000;
 
-app.use("/user" , );
+app.use("/user" , userRouter);
 
 app.listen(PORT , ()=>{
   console.log(`http://localhost:${PORT}`);
